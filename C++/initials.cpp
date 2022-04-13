9d5a44c639be5b32d8419d3fe9238c08fe86359e
Hero333 <86078262+Hero333@users.noreply.github.com>
2022-04-13 08:47:37 -0700

    Create initials.cpp

---
+++ C++/initials.cpp
@@ -0,0 +1,13 @@
+// g++ initials.cpp -o cwc.o
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << " CC    R R R     CC  \n";
+	cout << "C  C   R  R      C  C \n";
+	cout << "C      R R       C    \n";
+	cout << "C  C   R  R      C  C \n";
+	cout << " CC    R    R     CC  \n";
+	
+	return 0;
+}

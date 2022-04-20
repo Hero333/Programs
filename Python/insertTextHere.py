868cf957606bd5d126fd8ded49940d488b463d7f
Hero333 <86078262+Hero333@users.noreply.github.com>
2022-04-20 11:53:25 -0700

    Update insertTextHere.py

--- Python/insertTextHere.py
+++ Python/insertTextHere.py
@@ -13,8 +13,20 @@ if checkControls == "2":
     print("Unknown: So you must understand...")
     time.sleep(3)
     checkLieCC = input("Were you lying? \n 1: Yes\n 2: No\nYou: ")
+    if checkLieCC == "1":
+        askLieCC = input("Unknown: Why would you do that?\n 1: I felt like it\n 2: To see if I could\n 3: Your face is ugly\nYou: ")
+        if askLieCC == "1" or "2":
+            print("Unknown: Selfish whims")
+            checkReady = input("Unknown: Anyway are you ready? \n 1: Yes\n 2: Yes\n You: )
+        if askLieCC == "3":
+            print("You prick...")
+            print("We are starting now...")
     if checkLieCC == "2":
         print("Unknown: YOU HAVE TO BE!!!")
         time.sleep(2)
         print("Unknown: Sorry I was over reacting")
-        print("Unknown: I'm just going to asume you understand")
+        print("Unknown: I'm just going to assume you understand")
+        
+print("Unknown: Good.")
+time.sleep(1)
+print("Unknown: Then we can begin...")

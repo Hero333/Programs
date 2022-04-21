19e1c12ecdec2a5629423c9228655f9bb79c3eb0
Hero333 <86078262+Hero333@users.noreply.github.com>
2022-04-21 08:49:15 -0700

    Update insertTextHere.py

--- Python/insertTextHere.py
+++ Python/insertTextHere.py
@@ -1,32 +1,68 @@
 #! usr/bin/env python
+# Format may look weird as it is programed in an online IDE
+checkPoint = 0
+strength = 0
+resistance = 0
+magic = 0
+agility = 0
+language = 0
 
-import time,random
-print("Hello and welcome to [insert text here]")
-#time.sleep(5)
-print("Unknown: Hello, Player")
-print("Unknown: To play this game you will be given choices you will pick \n using numbers")
-checkControls = input("Unknown: Do you under stand? \n 1: Yes\n 2: No\nYou: ")
-
-if checkControls == "2":
-    print("Unknown: You just used them...")
-    time.sleep(3)
-    print("Unknown: So you must understand...")
-    time.sleep(3)
-    checkLieCC = input("Were you lying? \n 1: Yes\n 2: No\nYou: ")
-    if checkLieCC == "1":
-        askLieCC = input("Unknown: Why would you do that?\n 1: I felt like it\n 2: To see if I could\n 3: Your face is ugly\nYou: ")
-        if askLieCC == "1" or "2":
-            print("Unknown: Selfish whims")
-            checkReady = input("Unknown: Anyway are you ready? \n 1: Yes\n 2: Yes\n You: )
-        if askLieCC == "3":
-            print("You prick...")
-            print("We are starting now...")
-    if checkLieCC == "2":
-        print("Unknown: YOU HAVE TO BE!!!")
-        time.sleep(2)
-        print("Unknown: Sorry I was over reacting")
-        print("Unknown: I'm just going to assume you understand")
+def stats(points):
+    # Many errors with this function
+    end = True
+    pointsUsed = 0
+    while end:
+        print("Here are your stats: ")
+        print("\n-------------------")
+        print("Strength : " + str(strength) + "\nResistance : " + str(resistance) + "\n 3: ##### : " + str(magic) + "\nagility: " + str(agility) + "\nlanguage: " + str(language)") 
+        pointsUseable = points - pointsUsed 
+        spend = input("What would you like to add a point to?\n1: Strength\n2: Resistance 3: #####")
+while True:
+    import time,random
+    if checkPoint == 0:
+        print("Hello and welcome to [insert text here]")
+        print("Unknown: Hello, Player")
+        print("Unknown: To play this game you will be given choices you will pick \n using numbers")
+        checkControls = input("Unknown: Do you under stand? \n 1: Yes\n 2: No\nYou: ")
         
-print("Unknown: Good.")
-time.sleep(1)
-print("Unknown: Then we can begin...")
+        if checkControls == "2":
+            print("Unknown: You just used them...")
+            time.sleep(2)
+            print("Unknown: So you must understand...")
+            time.sleep(2)
+            checkLieCC = input("Were you lying? \n 1: Yes\n 2: No\nYou: ")
+            if checkLieCC == "1":
+                askLieCC = input("Unknown: Why would you do that?\n 1: I felt like it\n 2: To see if I could")
+                if askLieCC == "2" or "1":
+                    print("Unknown: Selfish whims")
+                    print("Unknown: However we can begin...")
+                    
+            if checkLieCC == "2":
+                print("Unknown: YOU HAVE TO BE!!!")
+                time.sleep(2)
+                print("Unknown: Sorry I was over reacting")
+                print("Unknown: I'm just going to assume you understand")
+                print("Let us begin...")
+                
+        if checkControls == "1":
+            print("Unknown: Good.")
+            print("Unknown: Then we can begin...")
+            
+        checkReady = input("Unknown: Are you ready? \n 1: Yes\n 2: Yes\n You: ")
+        
+        print("I suppose I should tell you what [insert text here] is")
+        checkPoint = checkPoint + 1
+        if checkPoint == 1:
+            print("[insert text here] is a game")
+            print("A game in which you will find all you could want")
+            print("There are skills in this game")
+            print("As well as stats")
+            print("We use turn based combat")
+            print("As it is civilized")
+            stats(0)
+            
+            
+            
+            
+            
+        

24384e055867a38827321032c344d4b4fbb6d169
Hero333 <86078262+Hero333@users.noreply.github.com>
2022-04-22 08:39:02 -0700

    Update insertTextHere.py

--- Python/insertTextHere.py
+++ Python/insertTextHere.py
@@ -1,23 +1,74 @@
 #! usr/bin/env python
 # Format may look weird as it is programed in an online IDE
+# Hours spent on program =~ 4 
 checkPoint = 0
-strength = 0
-resistance = 0
-magic = 0
-agility = 0
-language = 0
 
 def stats(points):
-    # Many errors with this function
+
+    global strength
+    global resistance
+    global magic
+    global agility
+    global language 
+    
+    strength = 0
+    resistance = 0
+    magic = 0
+    agility = 0
+    language = 0
+    
+    # Few errors with this function
     end = True
     pointsUsed = 0
+    pointsSpent = 0
     while end:
         print("Here are your stats: ")
         print("\n-------------------")
-        print("Strength : " + str(strength) + "\nResistance : " + str(resistance) + "\n 3: ##### : " + str(magic) + "\nagility: " + str(agility) + "\nlanguage: " + str(language)") 
-        pointsUseable = points - pointsUsed 
-        spend = input("What would you like to add a point to?\n1: Strength\n2: Resistance 3: #####")
-while True:
+        print("Strength : " + str(strength) + "\nResistance : " + str(resistance) + "\n##### : " + str(magic) + "\nagility: " + str(agility) + "\nlanguage: " + 
+        str(language)) 
+        print("-------------------\n")
+        pointsUseable = points - pointsSpent
+        spend = input("What would you like to add a point to? Or use 6 to leave\n1: Strength\n2: Resistance \n3: #####\n4: agility\n5: language\n6: Leave\nYou: ")
+        if spend == "1":
+            if pointsUseable >= 1:
+                strength = strength + 1
+                pointsSpent = pointsSpent + 1
+            else:
+                print("You do not have enough points for this")
+                break
+        if spend == "2":
+            if pointsUseable >= 1:
+                resistance = resistance + 1
+                pointsSpent = pointsSpent + 1
+            else:
+                print("You do not have enough points for this")
+                break
+        if spend == "3":
+            if pointsUseable >= 1:
+                magic = magic + 1
+                pointsSpent = pointsSpent + 1
+            else:
+                print("You do not have enough points for this")
+                break
+        if spend == "4":
+            if pointsUseable >= 1:
+                agility = agility + 1
+                pointsSpent = pointsSpent + 1
+            else:
+                print("You do not have enough points for this")
+                break
+        if spend == "5":
+            if pointsUseable >= 1:
+                language = language + 1
+                pointsSpent = pointsSpent + 1
+            else:
+                print("You do not have enough points for this")
+                break
+        if spend == "6":
+            break
+            
+stats(2)
+while False:
     import time,random
     if checkPoint == 0:
         print("Hello and welcome to [insert text here]")
@@ -60,9 +111,3 @@ while True:
             print("We use turn based combat")
             print("As it is civilized")
             stats(0)
-            
-            
-            
-            
-            
-        

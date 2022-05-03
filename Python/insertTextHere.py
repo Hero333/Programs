567e498c6bddee1b1e229bb31afd65058364f971
Jack Spencer <UniformKitten281@gmail.com>
2022-05-03 10:54:59 -0700

    Grammar and Understanding language fix

--- Python/insertTextHere.py
+++ Python/insertTextHere.py
@@ -58,29 +58,33 @@ def stats(points):
     # Few errors with this function
     pointsSpent = 0
     while True:
+        # I'm assuming that this is not Unknown talking,
+        # So I'm adding the name System to this.
+        print("System: ")
         print("Here are your stats: ")
         print("\n-------------------")
-        print("Strength : " + str(strength) + "\nResistance : " + str(resistance) + "\n##### : " + str(
+        print("Strength : " + str(strength) + "\nResistance : " + str(resistance) + "\n#####: " + str(
             magic) + "\nagility: " + str(agility) + "\nlanguage: " +
               str(language))
-        print("-------------------\n")
+        print("\n-------------------")
         pointsUsable = points - pointsSpent
-        print("You have " + str(pointsUsable) + " points")
+        print("You have " + str(pointsUsable) + " points.")
         spend = input(
-            "What would you like to add a point to? Or use 6 to leave\n1: Strength\n2: Resistance \n3: #####\n4: agility\n5: language\n6: Leave\nYou: ")
+            "What would you like to add a point to?\n1: Strength\n2: Resistance \n3: #####\n4: agility\n5: language\n6: Leave\nYou: ")
+        # Im not going to do it here, (yet) but i recommend that you turn this from checking for a string to checking for a number
         if spend == "1":
             if pointsUsable >= 1:
                 strength = strength + 1
                 pointsSpent = pointsSpent + 1
             else:
-                print("You do not have enough points for this")
+                print("You do not have enough points!")
                 break
         if spend == "2":
             if pointsUsable >= 1:
                 resistance = resistance + 1
                 pointsSpent = pointsSpent + 1
             else:
-                print("You do not have enough points for this")
+                print("You do not have enough points!")
                 break
         if spend == "3":
             if magicUnlock == "True":
@@ -88,26 +92,26 @@ def stats(points):
                     magic = magic + 1
                     pointsSpent = pointsSpent + 1
                 else:
-                    print("You do not have enough points for this")
+                    print("You do not have enough points!")
                     break
             else:
                 time.sleep(1)
                 print("⠀")
-                print("ERROR\n")
+                print("System Error!\n")
 
         if spend == "4":
             if pointsUsable >= 1:
                 agility = agility + 1
                 pointsSpent = pointsSpent + 1
             else:
-                print("You do not have enough points for this")
+                print("You do not have enough points!")
                 break
         if spend == "5":
             if pointsUsable >= 1:
                 language = language + 1
                 pointsSpent = pointsSpent + 1
             else:
-                print("You do not have enough points for this")
+                print("You do not have enough points!")
                 break
         if spend == "6":
             break
@@ -119,10 +123,10 @@ while True:
     import time, random
 
     if checkPoint == 0:
-        print("Hello and welcome to [insert text here]")
-        print("Unknown: Hello, Player")
-        print("Unknown: To play this game you will be given choices you will pick \n using numbers")
-        checkControls = input("Unknown: Do you under stand? \n 1: Yes\n 2: No\nYou: ")
+        print("Welcome to [insert text here]!")
+        print("Unknown: Hello, Player.")
+        print("Unknown: To play this game you will be given choices you will pick using numbers.")
+        checkControls = input("Unknown: Do you understand? \n 1: Yes\n 2: No\nYou: ")
 
         if checkControls == "2":
             print("Unknown: You just used them...")
@@ -131,54 +135,54 @@ while True:
             time.sleep(2)
             checkLieCC = input("Were you lying? \n 1: Yes\n 2: No\nYou: ")
             if checkLieCC == "1":
-                askLieCC = input("Unknown: Why would you do that?\n 1: I felt like it\n 2: To see if I could")
+                askLieCC = input("Unknown: Why would you do that?\n 1: I felt like it\n 2: To see if I could \nYou: ")
                 if askLieCC == "2" or "1":
-                    print("Unknown: Selfish whims")
+                    print("Unknown: Selfish whims!")
                     print("Unknown: However we can begin...")
 
             if checkLieCC == "2":
                 print("Unknown: YOU HAVE TO BE!!!")
                 time.sleep(2)
-                print("Unknown: Sorry I was over reacting")
-                print("Unknown: I'm just going to assume you understand")
-                print("Let us begin...")
+                print("Unknown: Sorry I was over-reacting.")
+                print("Unknown: I'm just going to assume you understand.")
+                print("Unknown: Let us begin...")
 
         if checkControls == "1":
             print("Unknown: Good.")
             print("Unknown: Then we can begin...")
 
-        checkReady = input("Unknown: Are you ready? \n 1: Yes\n 2: Yes\n You: ")
+        checkReady = input("Unknown: Are you ready?\n 1: Yes\n 2: Yes\nYou: ")
 
-        print("I suppose I should tell you what [insert text here] is")
+        print("Unknown: ")
+        print("I suppose I should tell you what [insert text here] is.")
         checkPoint = checkPoint + 1
         if checkPoint == 1:
-            print("[insert text here] is a game")
+            print("[insert text here] is a game!")
             time.sleep(2)
-            print("A game in which you will find all you could want")
+            print("A game in which you will find everything you have ever wanted.")
             time.sleep(2)
-            print("Where we use stats")
+            print("It even has a stats system!")
             time.sleep(1)
-            print("Here I'll show you your stats")
-            time.sleep(2)
-            print("I'll even give you some points")
+            print("I'll even give you some points for it.")
             stats(4)
-            print("If you didn't use some of those points")
+            print("If you don't spend any of the points,")
             time.sleep(1)
-            print("They are gone forever")
-            print("See")
-            stats(0)
-            print("So that would suck if you didn't spend some of those")
+            print("They are gone forever!")
             time.sleep(2)
-            print("Now its time for combat!!")
-            print("In combat if you attack and your opponent does the same damage will be delt")
-            print("Your strength subtracted by the opponents strength")
-            print("If your opponent has higher strength you will take damage equal to their strength subtracted by your resistance")
-            print("If you attack and your opponent blocks damage will be delt your resistance - opponents strength")
-            print("This works the same if you block and your opponent attacks")
-            print("If you dodge and are successful no damage will be taken")
-            print("Dodging works with a random number combined with your agility against a random number against opponents agility")
-            print("If a dodge and a block both happen or a dodge and a dodge or a block and a block")
-            print("Nothing happens")
-            print("You could always try to talk to your opponent but you'll need to speak their language")
-            print("try it out!")
+            print("Spend them now, or regret it forever!")
+            stats(0)
+            print("Well, hopefully you spent them.")
+            time.sleep(2)
+            print("Now its time for combat!")
+            print("In combat if you attack, and your opponent attacks too, damage will be dealt!")
+            print("The damage dealt is your strength substracted by your opponents strength.")
+            print("If your opponent has higher strength, the damage you take will be equal to their strength minus your resistance.")
+            print("If you opponent blocks your attack, damage will be delt. It will be your resistance minus the oppoents strength.")
+            print("This works the same if you block and your opponent attacks.")
+            print("If you dodge and are successful no damage will be taken.")
+            print("Dodging works with a random number combined with your agility against a random number against opponents agility.")
+            print("If a dodge and a block both happen or a dodge and a dodge or a block and a block.")
+            print("Nothing happens!")
+            print("You could always try to talk to your opponent but you'll need to speak their language.")
+            print("Try it out!")
             combat("Bill the Pill", 3, 2, 0, 1, "english", 100)
